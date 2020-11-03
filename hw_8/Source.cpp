c0851ed29eeaa9b459ffc8f791559efce7b47563
Jaelyn Sykes <jjsykes25@yahoo.com>
2020-11-03 17:29:25 -0600

    hw8p

--- hw_8/Source.cpp
+++ hw_8/Source.cpp
@@ -2,8 +2,15 @@
 using namespace std;
 
 int read_Data();
+int absolute_Value(int);
+bool check_Positive(int);
+bool odd_Even(int);
+void display(int);
 
 int main(void) {
+	int y = read_Data();
+	display(y);
+	return 0;
 
 }
 
@@ -15,9 +22,45 @@ int read_Data() {
 	return x;
 }
 
-bool check_Positive(){
-	if (x < 0)
-	{
-
+bool check_Positive(int x){
+	if (x > 0) {
+		return true;
 	}
+	else if (x = 0) {
+		return true;
+	}
+	else{
+		return false;
+	}
+
+}
+
+int absolute_Value(int x){
+	if (x >= 0) {
+		return x;
+	}
+	else {
+		int absolute = x - (x * 2);
+		cout << absolute;
+		return x;
+	}
+}
+
+bool odd_Even(int x) {
+	if (x = 0 && x % 2 == 0){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+void display(int x) {
+
+	cout << "Is the number positive? ";
+	cout << boolalpha << check_Positive(x) << endl;
+	cout << "The absolute value is ";
+	cout << absolute_Value(x) << endl;
+	cout << "Is the number even? ";
+	cout << odd_Even(x) << endl;
 }
