399393e7e26673605ac3c7fe036cdfc4fedb38f4
Jaelyn Sykes <jjsykes25@yahoo.com>
2020-11-03 17:24:37 -0600

    hw8.2

---
+++ hw_8/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int read_Data();
+
+int main(void) {
+
+}
+
+int read_Data() {
+	int x;
+
+	cout << "Enter a number: ";
+	cin >> x;
+	return x;
+}
+
+bool check_Positive(){
+	if (x < 0)
+	{
+
+	}
+}
